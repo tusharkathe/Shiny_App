bbe2a4fa0e99920b676e34a840bfc1049c802fc4
Tushar Kathe <tushar_kathe@yahoo.com>
2017-02-04 10:44:56 -0800

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,38 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+
+dataset <- diamonds
+
+fluidPage(
+  
+  titlePanel("Explore the Diamonds"),
+  
+  sidebarPanel(
+    
+    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
+                value=min(1000, nrow(dataset)), step=500, round=0),
+    
+    selectInput('x', 'X', names(dataset)),
+    selectInput('y', 'Y', names(dataset), names(dataset)[[2]]),
+    selectInput('color', 'Color', c('None', names(dataset))),
+    
+    checkboxInput('jitter', 'Jitter'),
+    checkboxInput('smooth', 'Smooth'),
+    
+    selectInput('facet_row', 'Facet Row', c(None='.', names(dataset))),
+    selectInput('facet_col', 'Facet Column', c(None='.', names(dataset)))
+  ),
+  
+  mainPanel(
+    plotOutput('plot')
+  )
+)
